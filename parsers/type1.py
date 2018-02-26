5a1f808e14218a1d1c227c09d8245f732400a6cb
Arthur Cheysson <arthur@cheysson.fr>
2018-02-26 19:48:52 +0100

    Finish modularization of regexes

--- parsers/type1.py
+++ parsers/type1.py
@@ -1,32 +1,42 @@
 import pandas as pd
 
-C_NOM = r"\w'().-"
-C_PRENOMS = r"\w',.-"
+R_MOT_SIMPLE = r"[{0}]+"
+R_MOT_COMPLEXE = r"[{0}][{1}]*"
+
+R_MOTS = r"{0}(?:(?: |\r)+{0})*"
+
+C_NOM_INIT = r"\w'(."
+C_NOM = r"\w'(),.-"
+C_PRENOMS_INIT = C_NOM_INIT
+C_PRENOMS = C_NOM
 C_USAGE = C_NOM
-C_LIEU = r"\w'/()[\]°.,:/-"
+C_LIEU = r"\w'/&()[\]°*.,:/-"
 C_DEP = r"\w'-"
 C_ADRESSE = C_LIEU
 C_VILLE =  C_LIEU
 
-R_NOM = fr"[{C_NOM}]+(?:(?: |\r)+[{C_NOM}]+)*"
-R_PRENOMS = fr"[{C_PRENOMS}]+(?:(?: |\r)+[{C_PRENOMS}]+)*"
-R_USAGE = fr"[{C_USAGE}]+(?:(?: |\r)+[{C_USAGE}]+)*"
+R_NOM = R_MOTS.format(R_MOT_COMPLEXE.format(C_NOM_INIT, C_NOM))
+R_PRENOMS = R_MOTS.format(R_MOT_COMPLEXE.format(C_PRENOMS_INIT, C_PRENOMS))
+R_USAGE = R_MOTS.format(R_MOT_SIMPLE.format(C_USAGE))
 R_DATE = r"\d{2}/\d{2}/\d{4}"
 
 # capturer en mode non gourmand, car un lieu peut se finir par un mot entre parenthèses, mais cela peut aussi être le
 # nom du département
-R_LIEU_NAISSANCE = fr"[{C_LIEU}]+(?:(?: |\r)+[{C_LIEU}]+)*?"
+R_LIEU_NAISSANCE = R_MOTS.format(R_MOT_SIMPLE.format(C_LIEU)) + "?"
 
-R_DEP = fr"[{C_DEP}]+(?:(?: |\r)+[{C_DEP}]+)*"
+R_DEP = R_MOTS.format(R_MOT_SIMPLE.format(C_DEP))
 
 REGEX_1 = fr"""^
 (?P<nom_famille>{R_NOM})(?: |\r)+-(?: |\r)+(?P<prenoms>{R_PRENOMS})\r
 (?:- +(?:\.|(?:[Ee][Pp]\. +)?(?P<nom_usage>{R_USAGE}))\r)?
-Né\(e\) le (?P<date_naissance>{R_DATE}) (?:à )?(?P<lieu_naissance>{R_LIEU_NAISSANCE})?(?:(?: |\r)*?\((?P<departement_naissance>{R_DEP})\))?
+Né\(e\) +le +(?P<date_naissance>{R_DATE}) (?:à )?(?P<lieu_naissance>{R_LIEU_NAISSANCE})?(?:(?: |\r)*?\((?P<departement_naissance>{R_DEP})\))?
 $""".replace('\n', '')
 
-REGEX_2 = r"""^
-(?P<adresse>[\w().°',:/-]+(?:(?: |\r)+[\w().°',:/-]+)*)(?: |\r)+(?P<code_postal>\d{4,5})(?P<ville>[\w'-]+(?:(?: |\r)[\w'-]+)*)
+R_ADRESSE = R_MOTS.format(R_MOT_SIMPLE.format(C_ADRESSE))
+R_VILLE = R_MOTS.format(R_MOT_SIMPLE.format(C_VILLE))
+
+REGEX_2 = fr"""^
+(?P<adresse>{R_ADRESSE})(?: |\r)+(?P<code_postal>\d{{2,5}})(?P<ville>{R_VILLE})
 (?:\r(?P<complement>[^\r]+))?
 $""".replace('\n', '')
 
