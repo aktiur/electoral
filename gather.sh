70f8ae5a2be2adea7fdf865c792b8ec9e9b01fc7
Arthur Cheysson <arthur@cheysson.fr>
2018-02-28 14:46:02 +0100

    Fix bug in gather when files have spaces in name

--- gather.sh
+++ gather.sh
@@ -5,4 +5,4 @@ dirname=`basename $1`
 filenames="$1/out/*.csv"
 files=( $filenames )
 
-( head -n 1 ${files[0]}; for i in "${files[@]}"; do tail -n +2 "$i"; done ) > $1/$dirname.csv
+( head -n 1 "${files[0]}"; for i in "${files[@]}"; do tail -n +2 "$i"; done ) > $1/$dirname.csv
