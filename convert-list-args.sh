6e753733e6c16f7c27517d4dd555bc30bbab37a5
Arthur Cheysson <arthur@cheysson.fr>
2018-05-21 17:50:09 +0200

    ???

--- convert-list-args.sh
+++ convert-list-args.sh
@@ -1,6 +1,8 @@
 BATCH_SIZE=500
 
-for file in $1/pdf/*.pdf; do
+cd "$1"
+
+for file in pdf/*.pdf; do
     name=$(basename "$file" .pdf)
     pages=$(pdfinfo "$file" | grep "Pages:" | awk '{print $2}')
     if (( pages > BATCH_SIZE ))
@@ -8,10 +10,10 @@ for file in $1/pdf/*.pdf; do
         typeset -i i
         batches=$(( pages / BATCH_SIZE ))
         for ((i=0; i<batches; i++)); do
-            printf '%s\0%s\0%s\0' "$file" $(( i*BATCH_SIZE + 1 ))-$(( (i+1)*BATCH_SIZE )) "$1/pretreated/${name}_$(printf "%03d" $i).csv"
+            printf '%s\0%s\0%s\0' "$1/$file" $(( i*BATCH_SIZE + 1 ))-$(( (i+1)*BATCH_SIZE )) "$1/pretreated/${name}_$(printf "%03d" $i).csv"
         done
-        printf '%s\0%s\0%s\0' "$file" $(( batches * BATCH_SIZE + 1 ))-$pages "$1/pretreated/${name}_$(printf "%03d" $batches).csv"
+        printf '%s\0%s\0%s\0' "$1/$file" $(( batches * BATCH_SIZE + 1 ))-$pages "$1/pretreated/${name}_$(printf "%03d" $batches).csv"
     else
-        printf '%s\0%s\0%s\0' "$file" all "$1/pretreated/${name}.csv"
+        printf '%s\0%s\0%s\0' "$1/$file" all "$1/pretreated/${name}.csv"
     fi
 done
--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CONCURRENCY=3
+CONCURRENCY=2
 
-mkdir -p $1/pretreated
+mkdir -p "$1/pretreated"
 
 ./convert-list-args.sh "$1" | xargs -0 -n 3 -P $CONCURRENCY ./convert-file.sh
